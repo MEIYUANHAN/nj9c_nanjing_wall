c9831a973e286908d660d6bae72b75754a3e9853
mer3450 <404261479@qq.com>
2026-05-12 20:15:23 +0800

    5.12*2

--- nanjing_wall_project/settings.py
+++ nanjing_wall_project/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'https://nj9cmcq.up.railway.app']
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
